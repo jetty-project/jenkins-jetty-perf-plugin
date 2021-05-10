089db009f8de20b1ae408f4b9b180ceccaeeeab0
olivier lamy <olamy@apache.org>
2021-05-10 11:54:56 +1000

    cleanup
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/org/mortbay/jetty/perf/jenkins/JdkPathFinderStep.java
+++ src/main/java/org/mortbay/jetty/perf/jenkins/JdkPathFinderStep.java
@@ -1,20 +1,19 @@
 package org.mortbay.jetty.perf.jenkins;
 
-import com.google.common.collect.ImmutableSet;
-import hudson.AbortException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
-import hudson.Launcher;
-import hudson.model.AbstractProject;
 import hudson.model.EnvironmentSpecific;
-import hudson.model.JDK;
 import hudson.model.Node;
-import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.slaves.NodeSpecific;
-import hudson.tasks.BuildStepDescriptor;
-import hudson.tasks.Builder;
 import hudson.tools.ToolDescriptor;
 import hudson.tools.ToolInstallation;
 import jenkins.model.Jenkins;
@@ -23,7 +22,6 @@ import org.apache.maven.toolchain.model.PersistedToolchains;
 import org.apache.maven.toolchain.model.ToolchainModel;
 import org.apache.maven.toolchain.model.io.xpp3.MavenToolchainsXpp3Writer;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
-import org.jenkinsci.Symbol;
 import org.jenkinsci.plugins.structs.SymbolLookup;
 import org.jenkinsci.plugins.workflow.steps.Step;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
@@ -35,14 +33,6 @@ import org.kohsuke.stapler.DataBoundSetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * generate a toolchain format file for each nodes/jdkname.
  * the generated file will be name `nodename`-toolchains.xml
@@ -50,14 +40,14 @@ import java.util.Set;
 public class JdkPathFinderStep extends Step
 {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger( JdkPathFinderStep.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdkPathFinderStep.class);
 
     private String[] nodes;
     private String[] jdkNames;
     private String propertiesFileSuffix;
 
     @DataBoundConstructor
-    public JdkPathFinderStep( String[] nodes, String[] jdkNames)
+    public JdkPathFinderStep(String[] nodes, String[] jdkNames)
     {
         this.nodes = nodes;
         this.jdkNames = jdkNames;
@@ -206,68 +196,9 @@ public class JdkPathFinderStep extends Step
 
     }
 
-    //@Override
-    public void performttt(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
-        LOGGER.info( "searching on nodes: {} for jdks: {}", Arrays.asList(nodes), Arrays.asList(jdkNames));
-        for (String nodeName : nodes)
-        {
-            PersistedToolchains persistedToolchains = new PersistedToolchains();
-
-            FilePath jdkPropertiesFile =
-                workspace.child(nodeName +
-                                    (StringUtils.isEmpty(propertiesFileSuffix)?"-toolchains.xml":propertiesFileSuffix));
-            Node node = "master".equals(nodeName)? Jenkins.get() : Jenkins.get().getNode(nodeName);
-            // if not search by label
-            if (node == null)
-            {
-                // we assume only one...
-                node = Jenkins.get().getNodes().stream()
-                    .filter(node1 -> node1.getLabelString().contains(nodeName))
-                    .findFirst()
-                    .orElse(null);
-            }
-
-            for (String jdkName : jdkNames)
-            {
-                JDK jdk = new JDK(jdkName, null);
-                jdk = jdk.forNode(node, listener);
-                listener.getLogger().println("node " + nodeName + ", jdkName:'" + jdkName + "' found jdk:" + jdk + "," + jdk.getClass());
-                String home = jdk.getHome();
-                if (StringUtils.isNotEmpty(home))
-                {
-                    ToolchainModel toolchainModel = new ToolchainModel();
-                    toolchainModel.setType("jdk");
-                    Xpp3Dom dom = new Xpp3Dom("configuration");
-                    Xpp3Dom jdkHome = new Xpp3Dom("jdkHome");
-                    jdkHome.setValue(home);
-                    dom.addChild(jdkHome);
-                    toolchainModel.setConfiguration(dom);
-                    persistedToolchains.addToolchain(toolchainModel);
-                }
-                else
-                {
-                    listener.error("node " + nodeName + " cannot find jdkHome for jdk: " + jdkName);
-                }
-            }
-            if (jdkPropertiesFile.exists())
-            {
-                jdkPropertiesFile.delete();
-            }
-            try (StringWriter stringWriter = new StringWriter())
-            {
-                MavenToolchainsXpp3Writer writer = new MavenToolchainsXpp3Writer();
-                writer.write(stringWriter, persistedToolchains);
-                jdkPropertiesFile.write().write(stringWriter.toString().getBytes(StandardCharsets.UTF_8));
-            }
-        }
-    }
-
-    //@Symbol("jdkpathfinder")
     @Extension
     public static final class DescriptorImpl extends StepDescriptor
     {
-
-
 
         @Override
         public String getDisplayName()
